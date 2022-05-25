1070f6c6b9041a2c30c9b210e8010dda4fda62da
rk365040 <rknaidu08@gmail.com>
2022-05-24 21:39:39 -0700

    Update deploy.sh

--- tomcat/template/deploy.sh
+++ tomcat/template/deploy.sh
@@ -4,6 +4,7 @@ HOSTNAME=`hostname`
 if [ "$HOSTNAME" = "ip-172.31.47.31" -o "$HOSTNAME" = "ip-172.31.39.213" ]; then
         ENV="dev"
         DEPLOY_PATH="/usr/share/tomcat/webapps/"
+        
 else
         echo "server not found"
 fi
@@ -12,4 +13,4 @@ cd /tmp
 wget https://tomcat.apache.org/tomcat-5.5-doc/appdev/sample/sample.war
 mv sample.war ram.war
 sudo chmod 777 ram.war
-sudo cp /tmp/ram.war $DEPLOY_PATH
+sudo cp /tmp/ram.war /usr/share/tomcat/webapps
